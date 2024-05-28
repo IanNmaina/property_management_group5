052bdc82a5fde169f533c6c307a39a43c41b37f1
Slozzyondul <solomonondula@gmail.com>
2024-05-28 13:32:46 +0300

    splashscreenisok

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// SplashScreen
+// SplashScreen (remain unchanged)
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();

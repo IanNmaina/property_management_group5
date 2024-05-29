c587ce85d07518039a2f76e2a1d505d9483bc889
Ian Maina <ianmainan21@gmail.com>
2024-05-29 12:17:10 +0300

    homepage widget

--- lib/classes/unit_category.dart
+++ lib/classes/unit_category.dart
@@ -1,14 +1,14 @@
-class Unit {
-  final String title;
-  final String imageUrl;
-  final int price;
+// class Unit {
+//   final String title;
+//   final String imageUrl;
+//   final int price;
 
-  Unit({required this.title, required this.imageUrl, required this.price});
-}
+//   Unit({required this.title, required this.imageUrl, required this.price});
+// }
 
-class Category {
-  final String name;
-  final List<Unit> units;
+// class Category {
+//   final String name;
+//   final List<Unit> units;
 
-  Category({required this.name, required this.units});
-}
+//   Category({required this.name, required this.units});
+// }
